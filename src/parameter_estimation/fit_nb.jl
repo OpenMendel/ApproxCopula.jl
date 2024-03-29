d7026b440e11da9580ee397bc6815b802e37f440
Benjamin Chu <benchu99@hotmail.com>
2024-03-29 14:25:36 -0700

    MOI for NB AR

--- src/parameter_estimation/fit_nb.jl
+++ src/parameter_estimation/fit_nb.jl
@@ -22,9 +22,9 @@ function fit!(
         gcm::NBCopulaVCModel,
         solver :: MOI.AbstractOptimizer = Ipopt.Optimizer();
         solver_config :: Dict = 
-            Dict("print_level"                => 5, 
+            Dict("print_level"                => 0, 
                  "tol"                        => 10^-3,
-                 "max_iter"                   => 100,
+                 "max_iter"                   => 15,
                  "accept_after_max_steps"     => 50,
                  "warm_start_init_point"      => "yes", 
                  "limited_memory_max_history" => 6, # default value
@@ -114,36 +114,62 @@ successive loglikelihood is less than `tol`.
 """
 function fit!(
     gcm::NBCopulaARModel,
-    solver=Ipopt.Ipopt.IpoptSolver(print_level = 0, max_iter = 15,
-                            limited_memory_max_history = 20,
-                            warm_start_init_point = "yes",  mu_strategy = "adaptive",
-                            hessian_approximation = "limited-memory");
+    solver :: MOI.AbstractOptimizer = Ipopt.Optimizer();
+    solver_config :: Dict = 
+        Dict("print_level"                => 0, 
+             "tol"                        => 10^-3,
+             "max_iter"                   => 15,
+             "accept_after_max_steps"     => 50,
+             "warm_start_init_point"      => "yes", 
+             "limited_memory_max_history" => 6, # default value
+             "hessian_approximation"      => "limited-memory",
+            #  "derivative_test"            => "first-order",
+             ),
     tol::Float64 = 1e-6,
     maxBlockIter::Int=10
     )
+    T = eltype(gcm.β)
+    solvertype = typeof(solver)
+    solvertype <: Ipopt.Optimizer ||
+        @warn("Optimizer object is $solvertype, `solver_config` may need to be defined.")
+
+    # Pass options to solver
+    config_solver(solver, solver_config)
+
+    # initial conditions
     initialize_model!(gcm)
     npar = gcm.p + 2 # rho and sigma squared
-    optm = MOI.NonlinearModel(solver)
-    # set lower bounds and upper bounds of parameters
-    lb   = fill(-Inf, npar)
-    ub   = fill(Inf, npar)
-    offset = gcm.p + 1
-    ub[offset] = 1
-    for k in 1:2
-        lb[offset] = 0
-        offset += 1
-    end
-    MOI.loadproblem!(optm, npar, 0, lb, ub, Float64[], Float64[], :Max, gcm)
-    # starting point
-    par0 = zeros(npar)
+    par0 = Vector{T}(undef, npar)
     modelpar_to_optimpar!(par0, gcm)
-    MOI.setwarmstart!(optm, par0)
-    logl0 = MOI.getobjval(optm)
+    solver_pars = MOI.add_variables(solver, npar)
+    for i in 1:npar
+        MOI.set(solver, MOI.VariablePrimalStart(), solver_pars[i], par0[i])
+    end
+
+    # constraints
+    solver.variables.lower[gcm.p + 1] = 0
+    solver.variables.upper[gcm.p + 1] = 1
+    solver.variables.lower[gcm.p + 2] = 0
+
+    # set up NLP optimization problem
+    # adapted from: https://github.com/OpenMendel/WiSER.jl/blob/master/src/fit.jl#L56
+    # I'm not really sure why this block of code is needed, but not having it
+    # would result in objective value staying at 0
+    lb = T[]
+    ub = T[]
+    NLPBlock = MOI.NLPBlockData(
+        MOI.NLPBoundsPair.(lb, ub), gcm, true
+    )
+    MOI.set(solver, MOI.NLPBlock(), NLPBlock)
+    MOI.set(solver, MOI.ObjectiveSense(), MOI.MAX_SENSE)
+    logl0 = MOI.eval_objective(gcm, par0)
+
     println("Converging when tol ≤ $tol (max block iter = $maxBlockIter)")
     # optimize
     for i in 1:maxBlockIter
-        MOI.optimize!(optm)
-        logl = MOI.getobjval(optm)
+        MOI.optimize!(solver)
+        modelpar_to_optimpar!(par0, gcm)
+        logl = MOI.eval_objective(gcm, par0)
         update_r!(gcm)
         if abs(logl - logl0) / (1 + abs(logl0)) ≤ tol # this is faster but has wider confidence intervals
             # if abs(logl - logl0) ≤ tol # this is slower but has very tight confidence intervals
@@ -154,10 +180,10 @@ function fit!(
             logl0 = logl
         end
     end
+
     # update parameters and refresh gradient
-    optimpar_to_modelpar!(gcm, MOI.getsolution(optm))
+    optimpar_to_modelpar!(gcm, MOI.get(solver, MOI.VariablePrimal(), solver_pars))
     loglikelihood!(gcm, true, false)
-    # gcm
 end
 
 """
